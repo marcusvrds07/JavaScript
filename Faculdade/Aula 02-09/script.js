9746e9e06632ac5a38f70b43a8627a40a7e0253f
Marcus Vinicius <165408887+marcusvrds07@users.noreply.github.com>
2024-09-03 00:12:39 +0000

    Calculadora e Aula de Função

---
+++ Faculdade/Aula 02-09/script.js
@@ -0,0 +1,62 @@
+// function nomeCompleto(nome,sobrenome) {
+//     return nome + " " + sobrenome
+// }
+
+// var nome = prompt("Digite seu nome: ");
+// var sobrenome = prompt("Digite seu sobrenome: ");
+
+// console.log(nomeCompleto(nome, sobrenome));
+
+
+
+
+// function nomes(n1,n2,n3) {
+    
+//     let nome1 = n1;
+//     let nome2 = n2;
+//     let nome3 = n3;
+
+//     if(nome1.length > nome2.length && nome1.length > nome3.length) {
+//         return n1;
+//     }
+//     else if(nome2.length > nome1.length && nome2.length > nome3.length) {
+//         return n2;
+//     }
+//     else {
+//         return n3;
+//     }
+
+// }
+
+// console.log(nomes("Marcus","Prredro","Lffffucas"));
+
+// let frase = prompt("Digite uma frase: ");
+
+// function frases(frase) {
+//     let f1 = frase
+//     let existe;
+//     if(f1.search("todos") !== -1 ) {
+//         existe = 1;
+//         return existe;
+//     }
+//     else {
+//         existe = 0;
+//         return existe;
+//     }
+// }
+
+// if(frases(frase)) {
+//     console.log('Existe "tudo" na frase');
+// }
+// else {
+//     console.log('Não existe "tudo" na frase');
+// }
+
+// let peso = parseFloat(prompt("Digite seu peso: "));
+// let altura = parseFloat(prompt("Digite sua altura: "));
+// console.log(imc(peso,altura));
+
+// function imc(peso,altura) {
+//     let imc = peso / (altura * altura);
+//     return imc;
+// }
